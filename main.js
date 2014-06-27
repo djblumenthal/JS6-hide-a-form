6b665519a9b9e28448b04c9ca32f0c856d673b3e
dana <djblumenthal@gmail.com>
2014-06-27 17:09:20 -0600

    more progress

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 $(document).on('ready', function() {
-  
+  //set value in input fields to text in profile sections 
+  $('#form-name').val(
+  //when submit/"save button" clicked:
+  $('#profile-edit').on('submit', function(e){
+  	
+  	//prevent page from refreshing
+  	e.preventDefault;
+  	
+  	//take values from input fields and save to corresponding profile sections
+
+  })
 });
